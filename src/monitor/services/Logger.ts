7c1dd596a60eab993c63566d8ba2103a2eb4573a
Edoardo Foco <e.foco@hotmail.it>
2023-04-26 20:28:09 +0200

    feat: logger

--- src/monitor/services/Logger.ts
+++ src/monitor/services/Logger.ts
@@ -1,6 +1,9 @@
+// import chalk from 'chalk';
 import { Service } from 'typedi';
 
 @Service()
 export class Logger {
-  info(message: string){ console.log(message);}
+    info(args: any){console.log(`[${new Date().toLocaleString()}] [INFO]`, args)};
+    warning(args: any){console.log(`[${new Date().toLocaleString()}] [WARN]`, args)};
+    error(args: any){console.log(`[${new Date().toLocaleString()}] [ERROR]`, args)};
 }
