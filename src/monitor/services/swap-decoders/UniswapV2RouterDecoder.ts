c4e2a10cd8625699a047a81edb9cd060b680909e
Edoardo Foco <e.foco@hotmail.it>
2023-04-30 11:12:53 +0200

    feat: uniswapv2 decoder handle swapExactTokensForTokens

--- src/monitor/services/swap-decoders/UniswapV2RouterDecoder.ts
+++ src/monitor/services/swap-decoders/UniswapV2RouterDecoder.ts
@@ -60,6 +60,12 @@ export class UniswapV2RouterDecoder implements ISwapDecoder {
                     blockNum: tx.blockNumber,
                     path: parsedTx.args[1]
                 }
+            case 'swapExactTokensForTokens':
+                return <ISwapTransactionDecoded>{
+                    hash: tx.hash,
+                    blockNum: tx.blockNumber,
+                    path: parsedTx.args[1]
+                }
                 
             default:
                this.logger.error(`UniswapV2 function non supported: ${parsedTx.name}. Hash: ${tx.hash}`);
