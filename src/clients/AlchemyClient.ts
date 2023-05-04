ceb4578052d97deeddf7859ee07e6dd0ca2a5b62
Edoardo Foco <e.foco@hotmail.it>
2023-05-04 08:28:05 +0200

    debug: add error logs on alchemy client

--- src/clients/AlchemyClient.ts
+++ src/clients/AlchemyClient.ts
@@ -1,19 +1,30 @@
 import { Inject, Service } from "typedi";
 import { Alchemy, TokenMetadataResponse, TransactionResponse } from "alchemy-sdk";
+import { Logger } from "../monitor/services/Logger";
 
 @Service()
 export class AlchemyClient {
     private readonly alchemy: Alchemy;
+    private readonly logger: Logger;
 
-    constructor(@Inject("alchemy") alchemy: Alchemy) {
+    constructor(@Inject("alchemy") alchemy: Alchemy, logger: Logger) {
         this.alchemy = alchemy;
+        this.logger = logger;
     }
 
     async getTransactionDetails(txHash: string) : Promise<TransactionResponse | null>{
-        return await this.alchemy.core.getTransaction(txHash);
+        const resp = await this.alchemy.core.getTransaction(txHash);
+        if(!resp){
+            this.logger.error(`Unable to get transaction details. Tx Hash: ${txHash}`);
+        }
+        return resp;
     }
 
     async getTokenMetadata(address: string) : Promise<TokenMetadataResponse | null>{
-        return await this.alchemy.core.getTokenMetadata(address);
+        const resp = await this.alchemy.core.getTokenMetadata(address);
+        if(!resp){
+            this.logger.error(`Unable to get token metadata. Token Address: ${address}`);
+        }
+        return resp;
     }
 }
